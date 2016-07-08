030e3cc927fad1ef0694c79f0edd8c4d6da1c6c5
Sam Thursfield <sam@afuera.me.uk>
2016-07-08 20:06:58 +0100

    Add basic functionality
    Can show the top artists by number of songs, and can annotate a playlist
    with URLs to files it can find.

--- calliope/tracker.py
+++ calliope/tracker.py
@@ -25,6 +25,8 @@ from gi.repository import Tracker
 import yaml
 
 import argparse
+import itertools
+import logging
 import sys
 
 import calliope
@@ -34,17 +36,53 @@ def argument_parser():
     parser = argparse.ArgumentParser(
         description="Create playlists from your local music collection, using "
                     "the Tracker database")
+    parser.add_argument('playlist', nargs='*',
+                        help="playlist file")
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help="enable verbose debug output")
+
+    parser.add_argument('--top', choices=['artists'],
+                        help="show the top artists (by number of songs)")
+
+    return parser
 
 
 class TrackerClient():
     def __init__(self):
-        self.conn = Tracker.SparqlConnection.get(None)
+        self._conn = Tracker.SparqlConnection.get(None)
+
+    def query(self, query):
+        logging.debug("Query: %s" % query)
+        return self._conn.query(query)
 
     def artist(self, artist_name):
         '''Return the Tracker URN for a given artist.'''
-        query_artist_urn = "SELECT ?u { ?u a nmm:MusicArtist ; nie:title \"%s\" }"
-        result = self.conn.query(query_artist_urn % artist_name)
-        return result[0]
+        query_artist_urn = "SELECT ?u { ?u a nmm:Artist ; dc:title \"%s\" }"
+        result = self.query(query_artist_urn % artist_name)
+
+        if result.next():
+            return result.get_string(0)[0]
+        else:
+            return None
+
+    def artists_by_number_of_songs(self, limit=1):
+        '''Return a list of artists by number of songs known.'''
+        query_artists_by_number_of_songs = """
+        SELECT ?artist_name (COUNT(?song) as ?songs)
+          { ?artist a nmm:Artist ;
+                dc:title ?artist_name .
+            ?song nmm:performer ?artist }
+        GROUP BY ?artist ORDER BY DESC(?songs)
+        LIMIT %i
+        """
+        result = self.query(query_artists_by_number_of_songs % limit)
+
+        while result.next():
+            artist_name = result.get_string(0)[0]
+            n_songs = result.get_string(1)[0]
+
+            print("%s: %s" % (artist_name, n_songs))
+
 
     def songs_for_artist(self, artist_id):
         '''Return all songs for a given artist.
@@ -62,7 +100,7 @@ class TrackerClient():
             nie:title(?track)
         WHERE {
             ?album a nmm:MusicAlbum ;
-                nmm:albumArtist %s .
+                nmm:albumArtist <%s> .
             ?track a nmm:MusicPiece ;
                 nmm:musicAlbum ?album
         } ORDER BY
@@ -76,40 +114,76 @@ class TrackerClient():
             nie:title(?track)
         WHERE {
             ?track a nmm:MusicPiece ;
-                nmm:artist %s .
-            OPTIONAL { ?track nmm:MusicAlbum ?album }
+                nmm:performer <%s> .
+            OPTIONAL { ?track nmm:musicAlbum ?album }
             FILTER (! bound (?album))
         } ORDER BY
             nie:title(?track)
         """
 
-        self.conn.query(
+        songs_with_releases = self.query(query_songs_with_releases % artist_id)
+        songs_without_releases = self.query(query_songs_without_releases % artist_id)
 
+        songs = []
+        while songs_with_releases.next():
+            songs.append(songs_with_releases.get_string(0)[0])
+        while songs_without_releases.next():
+            songs.append(songs_with_releases.get_string(0)[0])
+        return songs
 
 
 def expand(tracker, item):
     if 'artist' not in item:
         # This restriction may become annoying, hopefully we can relax it.
-        raise RuntimeError ('All items must specify at least "artist"')
+        raise RuntimeError ('All items must specify at least "artist": got %s'
+                            % item)
+
+    artist = tracker.artist(item['artist'])
+    if artist is None:
+        sys.stderr.write("Did not find any music for artist: %s\n" % item['artist'])
+        return
 
     if 'song' in item:
         # Get just the song
         pass
     else:
-        pass
+        print(list(tracker.songs_for_artist(artist)))
 
 
 def main():
-    input_playlist_data = yaml.safe_load(sys.stdin)
+    args = argument_parser().parse_args()
+    if args.debug:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
     tracker = TrackerClient()
 
-    for item in input_playlist_data:
-        if 'location' in item:
-            # This already has a URI!
-            pass
+    if args.top != None:
+        if args.top == 'artists':
+            tracker.artists_by_number_of_songs(limit=100)
+            return None
         else:
-            print(expand(tracker, item))
+            raise RuntimeError("Unknown type: %s" % args.top)
+
+    if args.playlist == None:
+        input_playlists = yaml.safe_load_all(sys.stdin)
+    else:
+        input_playlists = (yaml.safe_load(open(playlist, 'r')) for playlist in args.playlist)
+
+    for playlist in input_playlists:
+        data = None
+        if 'collection' in playlist:
+            data = playlist['collection']
+        elif 'list' in playlist:
+            data = playlist['list']
+        else:
+            raise RuntimeError ("Expected 'list' or 'collection' entry")
+
+        for item in data:
+            if 'location' in item:
+                # This already has a URI!
+                pass
+            else:
+                print(expand(tracker, item))
 
 
 try:
