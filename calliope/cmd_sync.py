3078ca78ed1ac8e9a20ecd93a5af13ce02f47c24
Sam Thursfield <sam@afuera.me.uk>
2017-11-15 22:38:10 +0000

    sync: Use Click for commandline argument parsing
    Writing a test suite made me realize that I'd totally forgotten
    this command.

--- calliope/cmd_sync.py
+++ calliope/cmd_sync.py
@@ -18,9 +18,9 @@
 '''calliope-sync'''
 
 
+import click
 import yaml
 
-import argparse
 import itertools
 import logging
 import os
@@ -30,38 +30,6 @@ import sys
 import warnings
 
 import calliope
-
-
-def argument_parser():
-    parser = argparse.ArgumentParser(
-        description="Copy playlists & collections between devices")
-    parser.add_argument('playlist', nargs='*',
-                        help="playlist file")
-    parser.add_argument('-d', '--debug', action='store_true',
-                        help="enable verbose debug output")
-    parser.add_argument('--dry-run', action='store_true',
-                        help="don't execute commands, only print what would "
-                             "be done")
-
-    parser.add_argument('--target', '-t', required=True,
-                        help="path to target device's filesystem")
-
-    parser.add_argument('--allow-formats', '-f', action='append',
-                        choices=['all', 'mp3'], default=[],
-                        help="specify formats that the target device can read;"
-                             " transcoding can be done if needed.")
-
-    parser.add_argument('--album-per-dir', action='store_true',
-                        help="organise the files on the target device so each "
-                             "album is in its own directory")
-    parser.add_argument('--number-dirs', action='store_true',
-                        help="ensure directory sort order matches desired "
-                             "playback order")
-    parser.add_argument('--number-files', action='store_true',
-                        help="ensure filename sort order matches desired "
-                             "playback order")
-
-    return parser
 
 
 class Operation():
@@ -156,45 +124,66 @@ def normalize_path(path):
     return ''.join([char if char in allowed else '_' for char in path])
 
 
-def main():
-    args = argument_parser().parse_args()
-    if args.debug:
+@calliope.cli.command(name='sync')
+@click.option('--dry-run', is_flag=True,
+              help="don't execute commands, only print what would be done")
+@click.option('--target', '-t', type=click.Path(exists=True, file_okay=False),
+              help="path to target device's filesystem")
+@click.option('--allow-formats', '-f', type=click.Choice(['all', 'mp3']),
+              multiple=True, default=[],
+              help="specify formats that the target device can read; "
+                   "transcoding can be done if needed.")
+@click.option('--album-per-dir', is_flag=True,
+              help="organise the files on the target device so each album is "
+                   "in its own directory")
+@click.option('--number-dirs', is_flag=True,
+              help="ensure directory sort order matches desired playback "
+                   "order")
+@click.option('--number-files', is_flag=True,
+              help="ensure filename sort order matches desired playback order")
+@click.argument('playlist', nargs=-1,
+                type=click.Path(exists=True, dir_okay=False))
+@click.pass_context
+def run(context, dry_run, target, allow_formats, album_per_dir,
+        number_dirs, number_files, playlist):
+    '''Copy playlists & collections between devices'''
+
+    if context.obj.debug:
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-    playlist_args = list(args.playlist)
-    if len(playlist_args) == 0:
+    if len(playlist) == 0:
         input_playlists = yaml.safe_load_all(sys.stdin)
     else:
-        input_playlists = (yaml.safe_load(open(playlist, 'r')) for playlist in playlist_args)
+        input_playlists = (yaml.safe_load(open(p, 'r')) for p in playlist)
 
-    allow_formats = args.allow_formats or ['all']
+    allow_formats = allow_formats or ['all']
 
     operations = []
     for playlist_data in input_playlists:
         for item_number, item in enumerate(calliope.Playlist(playlist_data)):
             if 'location' in item:
                 path = calliope.uri_to_path(item['location'])
-                if args.number_files:
+                if number_files:
                     filename = ensure_number(os.path.basename(path), item_number + 1)
                 else:
                     filename = None  # use existing
                 operations.append(
-                    sync_track(item['location'], args.target, allow_formats,
+                    sync_track(item['location'], target, allow_formats,
                                target_filename=filename))
             elif 'tracks' in item:
                 for track_number, track_item in enumerate(item['tracks']):
                     if 'location' in track_item:
                         path = calliope.uri_to_path(track_item['location'])
-                        if args.number_files:
+                        if number_files:
                             filename = ensure_number(
                                 os.path.basename(path),
                                 track_number + 1)
                         else:
                             filename = None  # use existing
 
-                        if args.album_per_dir:
+                        if album_per_dir:
                             album_name = item.get('album') or 'No album'
-                            if args.number_dirs:
+                            if number_dirs:
                                 dirname = make_dirname('%03i' % (item_number + 1),
                                                        item['artist'],
                                                        album_name)
@@ -215,12 +204,12 @@ def main():
                             target_dirname = None
 
                         operations.append(
-                            sync_track(track_item['location'], args.target,
+                            sync_track(track_item['location'], target,
                                        allow_formats,
                                        target_filename=target_filename,
                                        target_dirname=target_dirname))
 
-    if args.dry_run:
+    if dry_run:
         for operation in operations:
             print(operation)
     else:
