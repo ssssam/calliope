ssssam/calliope

commit 8233b45701cfe04fe9f23fb4cfb3cdbbce72fae9

Allow running CLI using `python -m calliope`
The main use case for this is so the test suite can launch the CLI
without having to be told where to find the generated `cpe` script
inside the Meson build directory.

calliope/__main__.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 # Calliope
-# Copyright (C) 2017  Sam Thursfield <sam@afuera.me.uk>
+# Copyright (C) 2017-2018  Sam Thursfield <sam@afuera.me.uk>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,20 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'''Calliope is a set of tools for processing playlists.
+import warnings
 
-This module is a wrapper script for launching Calliope from the path
-where it was installed.
+import calliope
 
-'''
+def pretty_warnings(message, category, filename, lineno,
+                    file=None, line=None):
+    return 'WARNING: %s\n' % (message)
 
-import click
+warnings.formatwarning = pretty_warnings
 
-import runpy
-import sys
 
-PYTHON_SITE_PACKAGES_DIR = '@pythondir@'
-
-sys.path.insert(1, PYTHON_SITE_PACKAGES_DIR)
-
-runpy.run_module('calliope')
+try:
+    calliope.cli()
+except RuntimeError as e:
+    sys.stderr.write("ERROR: %s\n" % e)
+    sys.exit(1)
+except BrokenPipeError as e:
+    # These happen when we are piped to `less` or something and are harmless.
+    sys.exit(0)

calliope/meson.build
@@ -1,5 +1,6 @@
 sources = [
   '__init__.py',
+  '__main__.py',
   'config.py',
   'cmd_export.py',
   'cmd_import.py',

commit 7ffb8157ff3d7827be4167a885410276762b544a

Fix missing import in __main__.py
Regression from 8233b45701cfe04fe9f23fb4cfb3cdbbce72fae9

calliope/__main__.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+import sys
 import warnings
 
 import calliope