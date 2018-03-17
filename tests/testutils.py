a8d21cd2780b078d79b876649ddae4b9b014efec
Sam Thursfield <sam@afuera.me.uk>
2018-03-17 00:27:41 +0000

    tests: Test real subprocesses instead of running commands in-process
    Something about the trackerappdomain library breaks when we try to run
    multiple app domains in the same session. Could be a bug in that library
    though.

--- tests/testutils.py
+++ tests/testutils.py
@@ -27,13 +27,6 @@ import subprocess
 import sys
 import traceback
 
-# We use pytest internals to capture the stdout/stderr during
-# a run of the Calliope CLI. We do this because click's
-# CliRunner convenience API (click.testing module) does not support
-# separation of stdout/stderr and also breaks the 'trackerappdomain' lib.
-#
-from _pytest.capture import MultiCapture, FDCapture
-
 import calliope
 
 
@@ -41,19 +34,14 @@ class Result():
 
     def __init__(self,
                  exit_code=None,
-                 exception=None,
-                 exc_info=None,
                  output=None,
                  stderr=None):
         self.exit_code = exit_code
-        self.exc = exception
-        self.exc_info = exc_info
         self.output = output
         self.stderr = stderr
 
     def assert_success(self, fail_message=''):
         assert self.exit_code == 0, fail_message
-        assert self.exc is None, fail_message
 
 
 class Cli():
@@ -71,60 +59,13 @@ class Cli():
         if result.stderr:
             print("Program stderr was:\n{}".format(result.stderr))
 
-            if result.exc_info and result.exc_info[0] != SystemExit:
-                traceback.print_exception(*result.exc_info)
-
         return result
 
-    @contextlib.contextmanager
-    def setup_input(self, text):
-        if text:
-            yield io.StringIO(text)
-        else:
-            with open(os.devnull) as devnull:
-                yield devnull
-
     def invoke(self, cli, args=None, input=None):
-        exc_info = None
-        exception = None
-        exit_code = 0
+        result = subprocess.run(
+            [sys.executable, '-m', 'calliope'] + args, encoding='utf8',
+            input=input, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        # Temporarily redirect sys.stdin to /dev/null to ensure that
-        # Popen doesn't attempt to read pytest's dummy stdin.
-        old_stdin = sys.stdin
-        with self.setup_input(text=input) as input_stream:
-            sys.stdin = input_stream
-
-            capture = MultiCapture(out=True, err=True, in_=False, Capture=FDCapture)
-            capture.start_capturing()
-
-            try:
-                cli.main(args=args or (), prog_name=cli.name)
-            except SystemExit as e:
-                if e.code != 0:
-                    exception = e
-
-                exc_info = sys.exc_info()
-
-                exit_code = e.code
-                if not isinstance(exit_code, int):
-                    sys.stdout.write('Program exit code was not an integer: ')
-                    sys.stdout.write(str(exit_code))
-                    sys.stdout.write('\n')
-                    exit_code = 1
-            except Exception as e:
-                exception = e
-                exit_code = -1
-                exc_info = sys.exc_info()
-            finally:
-                sys.stdout.flush()
-
-        sys.stdin = old_stdin
-        out, err = capture.readouterr()
-        capture.stop_capturing()
-
-        return Result(exit_code=exit_code,
-                      exception=exception,
-                      exc_info=exc_info,
-                      output=out,
-                      stderr=err)
+        return Result(exit_code=result.returncode,
+                      output=result.stdout,
+                      stderr=result.stderr)
