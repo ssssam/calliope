dc9456227dd87bd0521b84f3fb7c67ceb28baa2a
Sam Thursfield <sam@afuera.me.uk>
2018-08-15 13:44:31 +0200

    Add simple in/out test for `cpe export`

---
+++ tests/test_export.py
@@ -0,0 +1,39 @@
+# Calliope
+# Copyright (C) 2018  Sam Thursfield <sam@afuera.me.uk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import json
+
+
+def test_basic(cli):
+    input_tracks = [
+        { 'artist': 'Test1', 'start-time': 0 },
+        { 'artist': 'Test2', 'start-time': 50 }
+    ]
+
+    expected_output = '\n'.join([
+      "FILE \"none\" WAVE",
+      "  TRACK 01 AUDIO",
+      "    PERFORMER \"Test1\"",
+      "  INDEX 01 00:00:00",
+      "  TRACK 02 AUDIO",
+      "    PERFORMER \"Test2\"",
+      "  INDEX 01 00:50:00"])
+
+    result = cli.run(['export', '-'], input='\n'.join(json.dumps(track) for track in input_tracks))
+
+    assert result.exit_code == 0
+    assert result.output.strip() == expected_output
