ssssam/calliope

commit 5e70f8582d252e9dcf36a11ab6d43325af799dba

Add feature-disable options
This allows installing Calliope without having to install all of the
necessary dependencies. If the project grows to encompass all of the
things that I want it to do then this will become increasingly useful.

calliope/meson.build
@@ -1,24 +1,41 @@
 sources = [
-  '__init__.py',
   '__main__.py',
   'cache.py',
   'config.py',
   'cmd_export.py',
   'cmd_import.py',
-  'cmd_musicbrainz.py',
   'cmd_play.py',
   'cmd_shuffle.py',
-  'cmd_spotify.py',
   'cmd_stat.py',
-  'cmd_suggest.py',
   'cmd_sync.py',
-  'cmd_tracker.py',
   'cmd_web.py',
   'playlist.py',
 ]
 
+if get_option('musicbrainz')
+  sources += ['cmd_musicbrainz.py']
+endif
+
+if get_option('spotify')
+  sources += ['cmd_spotify.py']
+endif
+
+if get_option('suggest')
+  sources += ['cmd_suggest.py']
+endif
+
+if get_option('tracker')
+  sources += ['cmd_tracker.py']
+endif
+
 install_data(
     sources,
+    install_dir: join_paths(python_site_packages_dir, 'calliope'))
+
+configure_file(
+    input: '__init__.py.in',
+    output: '__init__.py',
+    configuration: cdata,
     install_dir: join_paths(python_site_packages_dir, 'calliope'))
 
 configure_file(

meson.build
@@ -1,6 +1,8 @@
 project('calliope')
 
-subproject('tracker-app-domain')
+if get_option('tracker')
+  subproject('tracker-app-domain')
+endif
 
 ###############################################################################
 # Python dependencies
@@ -32,10 +34,19 @@ endif
 
 install_requires = [
   'click',
-  'lightfm',
-  'musicbrainzngs',
-  'spotipy',
 ]
+
+if get_option('musicbrainz')
+  install_requires += ['musicbrainzngs']
+endif
+
+if get_option('spotify')
+  install_requires += ['spotipy']
+endif
+
+if get_option('suggest')
+  install_requires += ['lightfm']
+endif
 
 setup_requires = [
 ]
@@ -47,9 +58,17 @@ test_requires = [
 missing_dependency_errors = []
 
 message('Checking for Python dependencies')
-modules_check = run_command(python, 'meson-python-packages-check', install_requires + setup_requires + test_requires)
+modules_check = run_command(python, 'meson-python-packages-check', install_requires + setup_requires)
 if modules_check.returncode() != 0
   error('Python dependency requirements are not satisfied:\n' + modules_check.stderr().strip())
+endif
+
+if get_option('testsuite')
+  message('Checking for Python test suite dependencies')
+  modules_check = run_command(python, 'meson-python-packages-check', test_requires)
+  if modules_check.returncode() != 0
+    error('Test suite dependency requirements are not satisfied:\n' + modules_check.stderr().strip())
+  endif
 endif
 
 pytest = find_program('pytest-3', required: false)
@@ -63,6 +82,10 @@ pytest_workdir = meson.current_source_dir()
 
 cdata = configuration_data()
 cdata.set('pythondir', python_site_packages_dir)
+cdata.set('enable_musicbrainz', get_option('musicbrainz'))
+cdata.set('enable_spotify', get_option('spotify'))
+cdata.set('enable_suggest', get_option('suggest'))
+cdata.set('enable_tracker', get_option('tracker'))
 
 subdir('calliope')
 

calliope/__init__.py.in
@@ -21,7 +21,6 @@ This module contains the core parts of Calliope.
 '''
 
 import click
-import yaml
 
 import copy
 import enum
@@ -100,12 +99,20 @@ from . import playlist
 
 from . import cmd_export
 from . import cmd_import
-from . import cmd_musicbrainz
 from . import cmd_play
 from . import cmd_shuffle
-from . import cmd_spotify
 from . import cmd_stat
-from . import cmd_suggest
 from . import cmd_sync
-from . import cmd_tracker
 from . import cmd_web
+
+if @enable_musicbrainz@:
+    from . import cmd_musicbrainz
+
+if @enable_spotify@:
+    from . import cmd_spotify
+
+if @enable_suggest@:
+    from . import cmd_suggest
+
+if @enable_tracker@:
+    from . import cmd_tracker

commit eea0b99c6e4ea4267dbfffe45ff6be82bb182c35

Change how optional modules are imported
Rather than generating __init__.py from the template, we generate a JSON
file which is loaded by __init__.py at runtime.

This fixes the test suite, which was completely broken since commit
5e70f8582d252e9dcf36a11ab6d43325af799dba.

calliope/__init__.py
@@ -24,6 +24,7 @@ import click
 
 import copy
 import enum
+import json
 import logging
 import os
 import sys
@@ -68,20 +69,48 @@ from . import cmd_stat
 from . import cmd_sync
 from . import cmd_web
 
-if @enable_lastfm@:
+
+# Some modules can be switched on/off using `meson configure` when Calliope
+# is installed from a Git checkout. This makes it possible to use some features
+# without having to install every possible dependency.
+#
+# In order to discover which modules we should import, Meson generates a .json
+# file which we read here. Normally the file is installed alongside the module,
+# but when running the test suite from the source tree, the file will be inside
+# the build/ dir and the testsuite sets the CALLIOPE_MODULES_CONFIG variable
+# accordingly.
+#
+# Note that we can't simply generate the __init__.py file using Meson because
+# this makes it impossible to do `import calliope` from inside the source tree,
+# which would make `meson test` unusable.
+
+def load_modules_config():
+    if 'CALLIOPE_MODULES_CONFIG' in os.environ:
+        modules_config_file = os.environ['CALLIOPE_MODULES_CONFIG']
+    else:
+        module_path = os.path.dirname(__file__)
+        modules_config_file = os.path.join(module_path, 'modules.json')
+
+    with open(modules_config_file) as f:
+        return json.load(f)
+
+
+modules_config = load_modules_config()
+
+if modules_config['lastfm']:
     from . import cmd_lastfm
 
-if @enable_musicbrainz@:
+if modules_config['musicbrainz']:
     from . import cmd_musicbrainz
 
-if @enable_play@:
+if modules_config['play']:
     from . import cmd_play
 
-if @enable_spotify@:
+if modules_config['spotify']:
     from . import cmd_spotify
 
-if @enable_suggest@:
+if modules_config['suggest']:
     from . import cmd_suggest
 
-if @enable_tracker@:
+if modules_config['tracker']:
     from . import cmd_tracker

calliope/meson.build
@@ -1,4 +1,5 @@
 sources = [
+  '__init__.py',
   '__main__.py',
   'cache.py',
   'config.py',
@@ -64,8 +65,8 @@ install_data(
     install_dir: join_paths(python_site_packages_dir, 'calliope'))
 
 configure_file(
-    input: '__init__.py.in',
-    output: '__init__.py',
+    input: 'modules.json.in',
+    output: 'modules.json',
     configuration: cdata,
     install_dir: join_paths(python_site_packages_dir, 'calliope'))
 

meson.build
@@ -87,12 +87,14 @@ pytest_workdir = meson.current_source_dir()
 
 cdata = configuration_data()
 cdata.set('pythondir', python_site_packages_dir)
-cdata.set('enable_lastfm', get_option('lastfm'))
-cdata.set('enable_musicbrainz', get_option('musicbrainz'))
-cdata.set('enable_play', get_option('play'))
-cdata.set('enable_spotify', get_option('spotify'))
-cdata.set('enable_suggest', get_option('suggest'))
-cdata.set('enable_tracker', get_option('tracker'))
+cdata.set10('enable_lastfm', get_option('lastfm'))
+cdata.set10('enable_musicbrainz', get_option('musicbrainz'))
+cdata.set10('enable_play', get_option('play'))
+cdata.set10('enable_spotify', get_option('spotify'))
+cdata.set10('enable_suggest', get_option('suggest'))
+cdata.set10('enable_tracker', get_option('tracker'))
+
+top_build_dir = meson.current_build_dir()
 
 subdir('calliope')
 

tests/meson.build
@@ -7,6 +7,7 @@ tests = [
 ]
 
 pytest_env = environment()
+pytest_env.set('CALLIOPE_MODULES_CONFIG', join_paths(top_build_dir, 'calliope', 'modules.json'))
 # FIXME: doing 'prepend' twice seems to lose one of the values; possibly a
 # bug in Meson 0.44.1..
 pytest_env.prepend('PYTHONPATH', '@0@:@1@'.format(