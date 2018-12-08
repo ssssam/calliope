31b8a41114845c6206446d656758c2e2ef0af192
Sam Thursfield <sam@afuera.me.uk>
2018-12-08 01:07:39 +0100

    pylint: Ignore lastexport.py which is imported from elsewhere

--- calliope/lastfm/lastexport.py
+++ calliope/lastfm/lastexport.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+#pylint: disable=W,C
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
